a0dd49a133fa0312703be666564dc26e15eea252
kevinnadar <jesikamaraj@gmail.com>
2022-09-20 21:45:24 +0530

    Better Error Handling

--- shortzy/main.py
+++ shortzy/main.py
@@ -76,35 +76,29 @@ class Shortzy:
 
         is_short_link = await self.is_short_link(link)
 
-        if not is_short_link:
+        if is_short_link:
+            return link
+        if quick_link:
+            return await self.get_quick_link(url=link)
 
-            if quick_link:
-                return await self.get_quick_link(url=link)
+        params = {
+            self.__api_par: self.__api_key,
+            self.__url_par : link,
+            'alias': alias,
+            'format':'json'
+                }
+        try:
+            my_conn = aiohttp.TCPConnector(limit=10)
+            async with aiohttp.ClientSession(connector=my_conn) as session:
+                session = session
+                data = await self.__fetch(session, params)
 
-            else:  
-                params = {
-                    self.__api_par: self.__api_key,
-                    self.__url_par : link,
-                    'alias': alias,
-                    'format':'json'
-                        }
-                try:
-                    my_conn = aiohttp.TCPConnector(limit=10)
-                    async with aiohttp.ClientSession(connector=my_conn) as session:
-                        session = session     
-                        data = await self.__fetch(session, params)
-
-                        if data["status"] == "success":
-                            return data[self.__jsonpar]
-                        else:
-                            print(data['message'])
-                            return await self.__error_handler(url=link, silently_fail=silently_fail, exception=Exception)
-
-                except Exception as e:
-                    print(e)
-                    return await self.__error_handler(url=link, silently_fail=silently_fail, exception=Exception)
-
-        else: return link
+                if data["status"] == "success":
+                    return data[self.__jsonpar]
+                raise Exception(data['message'])
+                
+        except Exception as e:
+            raise Exception(e)
 
     async def get_quick_link(self, url:str, **kwargs) -> str:
         """
@@ -114,8 +108,7 @@ class Shortzy:
         :alias: The alias to use for the link
         :return: The converted links.
         """
-        link = self.qlink_par.format(base_site=self.__base_site, api_key=self.__api_key, url=url)
-        return link
+        return self.qlink_par.format(base_site=self.__base_site, api_key=self.__api_key, url=url)
 
 
     async def bulk_convert(self, urls:list, silently_fail:bool=True, quick_link:bool=False, **kwargs) -> list:
@@ -168,27 +161,8 @@ class Shortzy:
         :return: True if the link is a valid mdisk link, False otherwise
         """
         domain = urlparse(link).netloc
-        if self.__base_site in domain:
-            return True
-        return False
+        return self.__base_site in domain
 
-    async def __error_handler(self, url:str, silently_fail:bool, exception=Exception, message="Some error occurred during converting: %s"):
-        """
-        If the URL is valid, return it. If it's not, return it or raise an exception, depending on the value
-        of the `silently_fail` parameter
-        
-        :param url: The URL to be validated
-        :type url: str
-        :param silently_fail: If True, then if the URL is not valid, return the original URL. If False,
-        raise an exception
-        :type silently_fail: bool
-        :param exception: The exception to raise if the URL is not valid
-        :return: The url is being returned.
-        """
-        if silently_fail:
-            return url
-        else:
-            raise exception(message % url)
 
     async def __extract_url(self, string:str) -> list:
         regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
