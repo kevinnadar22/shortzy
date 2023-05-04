fdda268a22b17d7b167ec1f094ad7c2abca1a011
kevinnadar <jesikamaraj@gmail.com>
2023-05-04 23:10:39 +0530

    Upgraded to 0.0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.7'
+VERSION ='0.0.7'
 DESCRIPTION = 'An Unofficial Asynchronous Python version of Adlinkfly and Alternative Website API wrapper'
 
 # Setting up
